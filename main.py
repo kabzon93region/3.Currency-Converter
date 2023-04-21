03e5cc9a22a5d4a2331c66ac0418940a5291b98d
kabzon93region <65691103+kabzon93region@users.noreply.github.com>
2023-04-21 19:13:53 +0300

    1

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ vd.get_val_code_dict(dictval)
 
 for i, key in enumerate(dictval.keys()):
     print(f"{i+1}. {key}")
-
+valcode1: int
 while True:
     try:
-        valcode = int(input("Введите номер валюты из списка, курс которой вы хотите узнать: "))
-        if valcode > len(dictval.keys()) or valcode < 1:
+        valcode1 = int(input("Введите номер валюты первого числа из списка: "))
+        if valcode1 > len(dictval.keys()) or valcode1 < 1:
+            print("!ОШИБКА! : Вы ввели не существующий в списке номер валюты.")
+        else:
+            break
+    except ValueError:
+        print("!ОШИБКА! : Вы ввели не число.")
+
+
+valcode2: int
+while True:
+    try:
+        valcode2 = int(input("Введите номер валюты второго числа из списка: "))
+        if valcode2 > len(dictval.keys()) or valcode2 < 1:
             print("!ОШИБКА! : Вы ввели не существующий в списке номер валюты.")
         else:
             break
@@ -22,44 +34,16 @@ while True:
 value_list = list(dictval.values())
 
 # Запрос даты у пользователя
-while True:
-    try:
-        date_str = input("Введите дату в формате ДД.ММ.ГГГГ на которую вы хотите узнать курс валюты: ")
-        date = dt.datetime.strptime(date_str, "%d.%m.%Y")
-        date_str = date.strftime("%d/%m/%Y")
-        date = dt.datetime.strptime(date_str, "%d/%m/%Y")
-        date += dt.timedelta(days=1)
-        date_str = date.strftime("%d/%m/%Y")
-        break
-    except ValueError:
-        print("!ОШИБКА! : Вы ввели не дату или не в формате ДД.ММ.ГГГГ.")
 
-date_req: str = ""
 
-while True:
-    try:
-        date = dt.datetime.strptime(date_str, "%d/%m/%Y")
-        date -= dt.timedelta(days=1)
-        date_str = date.strftime("%d/%m/%Y")
-        date_req: str = date_str
 
-        url = "https://www.cbr.ru/scripts/XML_dynamic.asp"
-        params = {"date_req1": date_req, "date_req2": date_req, "VAL_NM_RQ": value_list[valcode-1]}
+print(vd.get_cur_on_date(value_list[valcode1 - 1]))
 
-        response = requests.get(url, params=params)
 
-        root = ET.fromstring(response.content)
-        date = root.attrib["DateRange1"]
-        currency_code = root.attrib["ID"]
-        value = root.find("Record/Value").text
-        break
-    except AttributeError:
-        print(f"На дату {date_req} не указан курс ЦБ, проверяем предыдущую дату")
+#nameval = [key for key, value in dictval.items() if value == currency_code]
+# nameval = str(nameval)
+# nameval = nameval.replace("[", "")
+#nameval = nameval.replace("'", "")
+#nameval = nameval.replace("]", "")
 
-nameval = [key for key, value in dictval.items() if value == currency_code]
-nameval = str(nameval)
-nameval = nameval.replace("[", "")
-nameval = nameval.replace("'", "")
-nameval = nameval.replace("]", "")
-
-print(f"Дата: {date}, Название валюты: {nameval}, Код валюты: {currency_code}, Курс: {value}")
+#print(f"Дата: {date}, Название валюты: {nameval}, Код валюты: {currency_code}, Курс: {value}")
--- valdict.py
+++ valdict.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import datetime as dt
 import requests
 
 def get_val_code_dict(cur_dict):
@@ -18,6 +19,35 @@ def get_val_code_dict(cur_dict):
         currency_dict[name] = code
     cur_dict.update(currency_dict)
 
-# Вывод словаря на экран
-#print(currency_dict)
 
+def get_cur_on_date(valcode: str):
+    while True:
+        try:
+            date_str = input("Введите дату в формате ДД.ММ.ГГГГ на которую вы хотите узнать курс валюты: ")
+            date = dt.datetime.strptime(date_str, "%d.%m.%Y")
+            date_str = date.strftime("%d/%m/%Y")
+            date = dt.datetime.strptime(date_str, "%d/%m/%Y")
+            date += dt.timedelta(days=1)
+            date_str = date.strftime("%d/%m/%Y")
+            break
+        except ValueError:
+            print("!ОШИБКА! : Вы ввели не дату или не в формате ДД.ММ.ГГГГ.")
+
+    while True:
+        try:
+            date = dt.datetime.strptime(date_str, "%d/%m/%Y")
+            date -= dt.timedelta(days=1)
+            date_str = date.strftime("%d/%m/%Y")
+            date_req: str = date_str
+
+            url = "https://www.cbr.ru/scripts/XML_dynamic.asp"
+            params = {"date_req1": date_req, "date_req2": date_req, "VAL_NM_RQ": valcode}
+
+            response = requests.get(url, params=params)
+
+            root = ET.fromstring(response.content)
+            value = root.find("Record/Value").text
+            return value
+            break
+        except AttributeError:
+            print(f"На дату {date_req} не указан курс ЦБ, проверяем предыдущую дату")
